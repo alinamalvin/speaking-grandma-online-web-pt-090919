233bc4421fb3034a6fdbe2d8e33fc478940fb3d3
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-09 21:06:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma method(phrase)
   if phrase.upcase==false 
     puts "HUH?! SPEAK UP, SONNY!"
   end
-else if phrase.upcase==true
+if phrase.upcase==true
     puts "NO, NOT SINCE 1938!"
   end
   else if phrase=
