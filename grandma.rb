1c5b17e6d770d9d669ddd7b1126c6fa58d42a011
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-09 21:07:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ if phrase.upcase==true
     puts "NO, NOT SINCE 1938!"
   end
   else if phrase="I LOVE YOU GRANDMA!"
-    
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
     else phrase.upcase==false 
     puts "HUH?! SPEAK UP, SONNY!"
   end
